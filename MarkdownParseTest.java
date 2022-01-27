9fe1c0bc24bb4e9c461b6c726b27f8408753eee6
lpechayco <lpechayc@ucsd.edu>
2022-01-27 14:10:44 -0800

    added list test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,19 @@
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    List strList = List.of("hi", "hey", "hello");
+
+    @Test
+    public void checkContents() {
+        assertEquals("hey", strList.get(1));
     }
 }
