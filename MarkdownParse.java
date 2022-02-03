41f7367061b8b38a9a8e9dece4ef308b022df11e
lpechayco <lpechayc@ucsd.edu>
2022-02-03 13:51:32 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,16 +13,12 @@ public class MarkdownParse {
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            if (nextOpenBracket == -1) {
-                break;
-            }
             int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")",openParen);
-            System.out.println("Next Open Bracket:" + nextOpenBracket);
-            System.out.println("next close bracket:" + nextCloseBracket);
-            System.out.println("open paren:" + openParen);
-            System.out.println("close paren:" + closeParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            int closeParen = markdown.indexOf(")", openParen);
+            if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
+            if(nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) != '!'){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
